f19fcf3da3fc7fbdbd1e6392badfb1c94b6f5507
glauberc <glaubercarv@gmail.com>
2019-11-11 15:54:58 -0300

    primeiro commit com app configurado

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './config/ReactotronConfig';
+import { View, Text } from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <View>
+      <Text>Lop Mobile</Text>
+    </View>
+  );
+};
+
+export default App;
